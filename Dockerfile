e7637f02fad5844c188a3396879614c9b222cf64
Raja Reddy <rajareddynetla@gmail.com>
2021-12-11 18:38:28 +0530

    java files

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM tomcat:8.0-jre8
+MAINTAINER Raja rajareddynetla@gmail
+COPY target/*.war /usr/local/tomcat/webapps/maven-web-project-1.0-SNAPSHOT.war
+EXPOSE 8080
+CMD ["catalina.sh", "run"]
---
+++ src/main/java/com/kc/contoller/SampleController.java
@@ -0,0 +1,34 @@
+package com.bt.contoller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+Testing comment
+**/
+
+@RestController
+@RequestMapping("/")
+public class SampleController {
+    @RequestMapping(value = "/getResponse", method = RequestMethod.GET)
+    @ResponseBody
+    public String printAddress(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+            throws JSONException {
+        int a = 20;
+System.out.println("Inside printAddress method");
+        JSONObject js = new JSONObject();
+        js.put("Name", "Raja");
+        js.put("Address", " Andhra");
+
+        return js.toString();
+
+    }
+}
